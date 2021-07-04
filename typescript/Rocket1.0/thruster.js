99f5a4c62aa610e01e2a336c7eb8ffbc3ee5dde2
Ramon Carles <ramon.carles@outlook.es>
2021-07-04 18:54:38 +0200

    'borrado'

--- typescript/Rocket1.0/thruster.js
+++
@@ -1,8 +0,0 @@
-var Thruster = /** @class */ (function () {
-    function Thruster(thrusterId, thrustMaxPower, currentThrust) {
-        this.thrusterId = thrusterId;
-        this.thrustMaxPower = thrustMaxPower;
-        this.currentThrust = currentThrust;
-    }
-    return Thruster;
-}());
