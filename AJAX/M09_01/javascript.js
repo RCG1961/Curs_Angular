0b93911bb1229b5cb588efd167d7e76fd80731d1
Ramon Carles <ramon.carles@outlook.es>
2021-07-19 18:32:27 +0200

    'ajax'

---
+++ AJAX/M09_01/javascript.js
@@ -0,0 +1,40 @@
+var index=-1;
+var index2=0;
+
+$(document).ready(function () {
+    $('#leerjoke').click(function (e) { 
+        e.preventDefault();
+        $('#chiste').html('');
+        $.get("random.json",function(data){
+           // console.log(data);
+           // console.log (data.length);                      // randon de chistes de Chuck Norris
+            if(index===-1){
+                joke= (data[0].value.joke)
+                $(".alert-primary").css("display", "block");
+                $('#chiste').html(joke).html()
+                index=data.length-1;
+                index=0;
+            }
+            else{
+                index =Math.floor(Math.random() * data.length);
+                while(index2 === index) {
+                    index =Math.floor(Math.random() * data.length);
+                }
+                index2=index;
+                joke= (data[index].value.joke)
+                $(".alert-primary").css("display", "block");
+                $('#chiste').html("Joke nº "+index+"<br>"+joke).html() 
+            }
+        });
+    });
+    $('#jokeapi').click(function (e) { 
+        e.preventDefault();
+        $('#chiste').html('');
+        $.get("http://api.icndb.com/jokes/random?",function(data){
+            //console.log(data);
+            //console.log(data.value.joke)
+            $(".alert-primary").css("display", "block");
+            $('#chiste').html("Joke de la API"+"<br>"+data.value.joke).html() 
+        });
+    });    
+});
---
+++ AJAX/javascript.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+    $('#leerEmpleados').click(function (e) { 
+        e.preventDefault();
+        $('#listaEmpleados').html('');
+        $.getJSON('http://localhost/ajax/empleados.php',
+        {"accion":"leer"},
+        function(data){
+            //console.log(data);
+            $.each(data, function (index, item) { 
+                 $('#listaEmpleados').html($('#listaEmpleados').html()+`
+                 <li><i class="fa fa-pencil-square-o" aria-hidden="true" 
+                 data-id='${item.id}' 
+                 data-nombre='${item.nombre}' 
+                 data-puesto='${item.puesto}' 
+                 data-edad='${item.edad}' 
+                 ></i> 
+                 <i class="fa fa-trash" aria-hidden="true" data-id='${item.id}' ></i>
+                 ${item.nombre} -- ${item.puesto} -- ${item.edad} </li>
+                 `);
+            });
+        });
+    });
+    $('#crearEmpleado').click(function (e) { 
+        e.preventDefault();
+        let nombre=$('#nombre').val();
+        let puesto=$('#puesto').val();
+        let edad=$('#edad').val();
+        $.post('http://localhost/ajax/empleados.php',
+        {"accion":"insertar","nombre":nombre,"puesto":puesto,"edad":edad},
+        function(data){
+            if(data==="1"){
+                $('#nombre').val('');
+                $('#puesto').val('');
+                $('#edad').val(''); 
+                $('#lista-tab').click();
+                $('#leerEmpleados').click();
+            }
+        });
+    });
+    $(document).on('click','.fa-pencil-square-o',function(){
+        //console.log($(this).data('id'));
+        $('#crearEmpleado').addClass('d-none');
+        $('#editarEmpleado').removeClass('d-none');
+        $('#datos-tab').html('Editar');
+        $('#datos-tab').click();
+        $('#nombre').val($(this).data('nombre'));
+        $('#puesto').val($(this).data('puesto'));
+        $('#edad').val($(this).data('edad'));
+        $('#id').val($(this).data('id'));
+
+    });
+    $(document).on('click','.fa-trash',function(){
+        let id=$(this).data('id');
+        const linea=$(this).parent();
+        //console.log(id);
+        $.post("http://localhost/ajax/empleados.php", 
+            {"accion":"borrar","id":id},
+            function (data) {
+                //console.log(data);
+                linea.remove();
+            }
+        );
+    });
+    
+    $('#editarEmpleado').click(function (e) { 
+        e.preventDefault();
+        let nombre=$('#nombre').val();
+        let puesto=$('#puesto').val();
+        let edad=$('#edad').val();
+        let id=$('#id').val();
+        $.post('http://localhost/ajax/empleados.php',
+        {"accion":"editar","nombre":nombre,"puesto":puesto,"edad":edad,"id":id},
+        function(data){
+            if(data=='1'){
+
+            }
+        });
+
+        $('#lista-tab').click();
+        $('#leerEmpleados').click();
+        $('#crearEmpleado').removeClass('d-none');
+        $('#editarEmpleado').addClass('d-none');
+    });
+
+
+});
+
